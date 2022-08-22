c8e06e02aabdf21e7fee25685c9eb57a2f71a693
amberocia <84581567+amberocia@users.noreply.github.com>
2022-08-22 21:38:53 +0200

    Added some media screen stuff

--- app.js
+++ app.js
@@ -32,3 +32,15 @@ function backToTop(){
     window.scrollTo(0,0);
 }
 
+//Sidebar
+const sideMenu=document.querySelector("aside");
+const menuBtn=document.querySelector("#menu_btn");
+const closeBtn=document.querySelector("#close_btn");
+
+menuBtn.addEventListener('click',()=>{
+    sideMenu.style.display='block'
+})
+
+closeBtn.addEventListener('click',()=>{
+    sideMenu.style.display='none'
+})
